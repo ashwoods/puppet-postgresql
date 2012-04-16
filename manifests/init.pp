76d3c2b7575cc899ef9f5dcb0355d4cb12396bb2
Ashley Camba Garrido <ashwoods@gmail.com>
2012-04-16 17:29:47 +0200

    fixed postgresql for oneiric

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,17 @@
 class postgresql {
   case $operatingsystem {
-    Debian: { 
+    Debian: {
       case $lsbdistcodename {
         lenny :  { include postgresql::debian::v8-3 }
         squeeze: { include postgresql::debian::v8-4 }
         default: { fail "postgresql not available for ${operatingsystem}/${lsbdistcodename}"}
       }
-    } 
+    }
     Ubuntu: {
       case $lsbdistcodename {
         lucid :  { include postgresql::debian::v8-4 }
-        precise, oneiric: { include postgresql::ubuntu::v9-1 }
+        oneiric: { include postgresql::ubuntu::v9-1 }
+        precise: { include postgresql::ubuntu::v9-1 }
         default: { fail "postgresql not available for ${operatingsystem}/${lsbdistcodename}"}
       }
     }
